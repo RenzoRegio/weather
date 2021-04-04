42e0e1a97978b7906be770e68b1991c7909fac7b
RenzoRegio <renzorregio@gmail.com>
2021-04-03 20:59:57 -0700

    Add Loading Page

---
+++ Loading.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { StyleSheet, Text, View } from "react-native";
+
+export default () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Getting the weather</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "teal",
+    justifyContent: "flex-end",
+    paddingHorizontal: 20,
+    paddingVertical: 120,
+  },
+  text: {
+    fontSize: 45,
+    color: "white",
+    width: "70%",
+  },
+});
