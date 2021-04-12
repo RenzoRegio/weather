50ecf155d30cde2c22243628a3a1cb99a5e7618b
RenzoRegio <renzorregio@gmail.com>
2021-04-11 18:45:10 -0700

    Add Location

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import * as Location from "expo-location";
 
 import Loading from "./Loading";
-export default function App() {
-  return <Loading />;
+
+export default class extends React.Component {
+  getLocation = async () => {
+    const permission = await Location.requestPermissionsAsync();
+    const location = await Location.getCurrentPositionAsync();
+    console.log(location);
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    return <Loading />;
+  }
 }
